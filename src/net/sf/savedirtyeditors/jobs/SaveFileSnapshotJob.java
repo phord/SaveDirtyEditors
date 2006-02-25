08a261c35a6655078e86232ce9ac1db91f6575c7
vraravam <vraravam>
2006-02-25 00:17:15 +0000

    Adding javadocs.

--- src/net/sf/savedirtyeditors/jobs/SaveFileSnapshotJob.java
+++ src/net/sf/savedirtyeditors/jobs/SaveFileSnapshotJob.java
@@ -25,6 +25,10 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.ui.IFileEditorInput;
 import org.osgi.framework.Bundle;
 
+/**
+ * A custom implementation of {@link Job} to schedule either the {@link SaveFileSnapshotAction} or the
+ * {@link DeleteFileSnapshotAction}. This belongs to the Job family defined by {@link PluginActivator#JOB_FAMILY_NAME}.
+ */
 public final class SaveFileSnapshotJob extends Job {
     private static final long RESCHEDULE_DELAY = 300000;// 5 minutes
 
@@ -33,6 +37,12 @@ public final class SaveFileSnapshotJob extends Job {
     private final IFileEditorInput fileEditorInput;
     private boolean completed = false;
 
+    /**
+     * Constructor for SaveFileSnapshotJob.
+     * 
+     * @param fileEditorInput
+     *            The non-null {@link IFileEditorInput} for which the scheduled tasks need to be run.
+     */
     public SaveFileSnapshotJob(final IFileEditorInput fileEditorInput) {
         super(fileEditorInput.getToolTipText());
         this.fileEditorInput = fileEditorInput;
@@ -41,18 +51,45 @@ public final class SaveFileSnapshotJob extends Job {
         schedule();
     }
 
+    /**
+     * Returns true if family is the same as defined by {@link PluginActivator#JOB_FAMILY_NAME}
+     * 
+     * @see Job#belongsTo(Object)
+     */
     public boolean belongsTo(final Object family) {
         return PluginActivator.JOB_FAMILY_NAME.equals(family);
     }
 
-    public boolean isForInput(IFileEditorInput fileEditorInput) {
+    /**
+     * Returns true if the specified {@link IFileEditorInput} is the <b>same</b> as the one that this Job was created
+     * for.
+     * 
+     * @param fileEditorInput
+     *            The IFileEditorInput that needs to be tested
+     * @return True if the <code>fileEditorInput</code> is the same exact instance as the one that this Job was
+     *         created for; false otherwise
+     */
+    public boolean isForInput(final IFileEditorInput fileEditorInput) {
         return this.fileEditorInput == fileEditorInput;
     }
 
+    /**
+     * @see Job#shouldRun()
+     */
     public boolean shouldRun() {
         return !completed;
     }
 
+    /**
+     * @see Job#shouldSchedule()
+     */
+    public boolean shouldSchedule() {
+        return !completed;
+    }
+
+    /**
+     * @see Job#run(IProgressMonitor)
+     */
     protected IStatus run(final IProgressMonitor monitor) {
         // if the system is shutting down, don't build
         if (systemBundle.getState() == Bundle.STOPPING) {
@@ -73,6 +110,10 @@ public final class SaveFileSnapshotJob extends Job {
         return Status.OK_STATUS;
     }
 
+    /**
+     * Called when this Job needs to be shutdown. This method will run the {@link DeleteFileSnapshotAction} for the
+     * <code>fileEditorInput</code> from this Job and then stop itself from being scheduled/run any more.
+     */
     public void complete() {
         PluginActivator.logDebug("Completing snapshot for: " + fileEditorInput.getToolTipText()); //$NON-NLS-1$
         // Need to clean the temp area
@@ -82,7 +123,7 @@ public final class SaveFileSnapshotJob extends Job {
         cancel();
     }
 
-    private void run(ISafeRunnable runnable) {
+    private void run(final ISafeRunnable runnable) {
         // HACKTAG: Once we move to 3.2, remove deprecation
         Platform.run(runnable);
     }
